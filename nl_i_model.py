82550b5c79ebd022afbab20b213f60d8dbd6b51b
Jackson Karel <jackson.karel2@gmail.com>
2021-07-17 13:23:03 -0700

    Handle more sentence types

--- nl_i_model.py
+++ nl_i_model.py
@@ -8,30 +8,23 @@ def i_var_format(arg_par, i_lem, parenthesis="("):
     return i_lem
 
 def i_concat_children(token, i_lem):
-    conjs = []
     child_deps = [child.dep_ for child in token.children]
-    nonpobj_vars = ["det", "nummod", "amod", "prep"]
+    noun_deps = ["det", "nummod", "amod", "nmod", "advmod", "poss", "appos", "prep", "acl", "relcl", "cc", "conj"]
     dep_i_var_dict = {
-        "prep": ["pobj"],
-        "pobj": ["det", "nummod", "amod", "prep"],
-        "nsubj": nonpobj_vars,
-        "nsubjpass": nonpobj_vars,
-        "dobj": nonpobj_vars,
-        "ccomp": ["aux", "auxpass", "advmod", "prep", "nsubj", "nsubjpass", "dobj"],
-        "amod": ["advmod"],
-        "acomp": ["advmod"],
-        "relcl": ["aux", "auxpass", "advmod", "ccomp", "acomp", "prep", "nsubj", "nsubjpass", "dobj"],
-        "aux": ["neg", "acomp"]
+        "ADP": ["pobj", "cc", "conj"],
+        "NOUN": noun_deps,
+        "PRON": noun_deps,
+        "VERB": ["neg", "aux", "auxpass", "advmod", "npadvmod", "ccomp", "acomp", "xcomp", "prep", "mark", "nsubj", "nsubjpass", "dobj", "pobj", "agent", "advcl", "cc", "conj"],
+        "ADJ": ["advmod", "amod", "cc", "conj"],
+        "ADV": ["advmod", "prep"],
+        "AUX": ["neg", "acomp", "xcomp", "attr", "nsubj"],
+        "DET": ["prep", "pobj"]
     }
     il_vars = [key for key in dep_i_var_dict]
-    non_vars = ["aux", "punct", "compound", "mark", "conj", "cc"]
-    root_verb = token.dep_ == "ROOT" and token.pos_ == "VERB"
-    dep_in_vars = token.dep_ in il_vars
-    if root_verb or dep_in_vars:
-        if root_verb:
-            dep_i_vars = ["aux", "auxpass", "advmod", "ccomp", "acomp", "prep", "nsubj", "nsubjpass", "dobj"]
-        else:
-            dep_i_vars = dep_i_var_dict[token.dep_]
+    non_vars = ["punct", "compound"]
+    dep_in_vars = token.pos_ in il_vars
+    if dep_in_vars:
+        dep_i_vars = dep_i_var_dict[token.pos_]
         expected_vars = dep_i_vars + non_vars
         for idx, i_var in enumerate(dep_i_vars):
             for child in token.children:
@@ -50,9 +43,6 @@ def i_concat_children(token, i_lem):
     elif token.dep_ not in non_vars:
         print("New token type:", token.text, token.pos_, token.dep_)
 
-    for idx, child in enumerate(token.children):
-        if child.dep_ == "cc":
-            conjs.append(child) 
     i_lem = i_lem + ")"
     return i_lem
 
@@ -82,19 +72,11 @@ def doc_i_lemma(token, i_lem):
     return i_lem
 
 nlp = spacy.load('en_core_web_trf')
-doc = nlp("These statistical facts have been adequately documented elsewhere and will not concern us here.")
+doc = nlp("The scope and impact of this cultural change have received some attention, but no attempt has yet been made to understand its basic mechanism.")
 
 full_span = doc[0:]
 fs_root = full_span.root
 i_lem = ""
-i_lem = doc_i_lemma(fs_root, i_lem)
-for token in fs_root.children:
-    if token.dep_ == "cc":
-        ands = ("and", "but", "yet", "as")
-        ors = ("or")
-        cc_mapping = {
-            ands: "\<and>",
-            ors: "\<or>"
-        }
+i_lem = doc_i_lemma(fs_root, i_lem)    
 
     
