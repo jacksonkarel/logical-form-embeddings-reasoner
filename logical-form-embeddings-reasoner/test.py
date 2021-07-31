02c104692b2b55d2be90965daa64ff4230f60e5f
Jackson Karel <jackson.karel2@gmail.com>
2021-07-31 13:09:00 -0700

    Remove while loop and displacy from test script

--- logical-form-embeddings-reasoner/test.py
+++ logical-form-embeddings-reasoner/test.py
@@ -2,9 +2,10 @@ from spacy import displacy
 
 from nl_to_lf import ecw_trf_doc, doc_i_lemma
 
-while True:
-    text = input("Enter your text: ")
-    doc = ecw_trf_doc(text)
-    i_lem = doc_i_lemma(doc)
-    print(i_lem)
-    displacy.serve(doc, style="dep")
+# while True:
+text = input("Enter your text: ")
+doc = ecw_trf_doc(text)
+i_data = doc_i_lemma(doc)
+i_lem = i_data["lemma"]
+print(i_lem)
+    # displacy.serve(doc, style="dep")
