3fd4f14e8b7c51706527d040610d8b0a57f1359f
Jackson Karel <jackson.karel2@gmail.com>
2021-07-31 13:12:27 -0700

    Isabelle testing

--- Scratch2.thy
+++ Scratch2.thy
@@ -4,10 +4,11 @@ theory Scratch2
   imports Main
 begin
 
-lemma "(\<forall>e f g. is_aux(e, g, f(g, the_det)) \<longrightarrow>  e = f(g, the_det))
-\<and> is_aux(change_noun, param1, actualization_noun(None, the_det)) \<and>
-is_aux(change_noun, None, feature_noun(real_adj, a_det, of_adp(world_noun(None, the_det, None, None))))
-\<longrightarrow> is_aux(actualization_noun(None, the_det), None, feature_noun(real_adj, a_det, of_adp(world_noun(None, the_det, None, None))))"
+lemma "(\<forall>e f g. is_aux(e, g, f(g, the_det)) \<longrightarrow>  e = f(g, the_det)) \<and>
+is_aux(change_noun, param1, actualization_noun(param1, the_det)) \<and>
+is_aux(change_noun, None, feature_noun(real_adj, a_det, of_adp(world_noun(None, the_det, None, None)))) \<and>
+change_noun(hello)
+\<longrightarrow> is_aux(actualization_noun(param1, the_det), None, feature_noun(real_adj, a_det, of_adp(world_noun(None, the_det, None, None))))"
   by auto
 
 
