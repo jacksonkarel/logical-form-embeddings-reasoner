fadf0acec3f69ddad9954b385e11bdd9eab07297
Jackson Karel <jackson.karel2@gmail.com>
2021-08-28 14:00:57 -0700

    Update Test isabelle theory

--- theories/Test.thy
+++ theories/Test.thy
@@ -5,6 +5,7 @@ theory Test
 begin
 
 lemma "(\<forall>e f g. is_aux(e, g, f(g, the_det)) \<longrightarrow>  e = f(g, the_det)) \<and>
+e \<noteq> None \<and>
 is_aux(change_noun, param1, actualization_noun(param1, the_det)) \<and>
 is_aux(change_noun, None, feature_noun(real_adj, a_det, of_adp(world_noun(None, the_det, None, None)))) \<and>
 change_noun(hello)
